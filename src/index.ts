f41e48161aa9157a7d89f1d1fe8619941223ac23
martynasj <lyrinisobjektas@gmail.com>
2018-01-09 17:43:42 +0000

    test

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import * as Tickers from './tickers'
+// import * as Tickers from './tickers'
 
-const updateIntervalInMins = 10
+// const updateIntervalInMins = 10
 
-// Process Starts Here
-function main() {
-  // initRefetchingProcess()
-  Tickers.updateTickers()
-}
+// // Process Starts Here
+// function main() {
+//   // initRefetchingProcess()
+//   Tickers.updateTickers()
+// }
 
-function initRefetchingProcess() {
-  Tickers.updateTickers({ limit: 500 })
-  setInterval(() => {
-    Tickers.updateTickers()
-  }, updateIntervalInMins * 60 * 1000)
-}
+// function initRefetchingProcess() {
+//   Tickers.updateTickers({ limit: 500 })
+//   setInterval(() => {
+//     Tickers.updateTickers()
+//   }, updateIntervalInMins * 60 * 1000)
+// }
 
-main()
+// main()
+console.log('test')
