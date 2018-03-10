ef453b2e4768ee9d9ef4ca3649ebdb6e61329d77
Martynas J <lyrinisobjektas@gmail.com>
2018-03-10 16:14:22 +0000

    Add atob and dotenv packages

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,13 @@
-var admin = require('firebase-admin')
+const admin = require('firebase-admin')
+const atob = require('atob')
+// locally we read from .env but on heroku env everything should be set in env variables
+require('dotenv').config()
 
-var serviceAccount = require('../fb-key.json')
+const firebaseCred = JSON.parse(atob(process.env.FIREBASE_CRED))
 
 export function init() {
   const config = {
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(firebaseCred),
     databaseURL: 'https://shit-coin-portfolio.firebaseio.com',
     storageBucket: 'shit-coin-portfolio.appspot.com',
   }
